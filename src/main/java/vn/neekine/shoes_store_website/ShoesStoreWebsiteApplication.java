451ec7a30a7fa9744b43f8a45c50559ddc02e08b
Nekine <kien12045@gmail.com>
2024-07-07 16:27:06 +0700

    create project

---
+++ src/main/java/vn/neekine/shoes_store_website/ShoesStoreWebsiteApplication.java
@@ -0,0 +1,13 @@
+package vn.neekine.shoes_store_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesStoreWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesStoreWebsiteApplication.class, args);
+	}
+
+}
