f8d822c61c587ffbb1dc468e955382efe7d7175e
Nekine <kien12045@gmail.com>
2024-07-07 16:27:06 +0700

    create project

---
+++ src/test/java/vn/neekine/shoes_store_website/ShoesStoreWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.neekine.shoes_store_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesStoreWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
