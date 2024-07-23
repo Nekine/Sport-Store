9169fed01844336118e7e3375cd252b748f9e2de
Nekine <kien12045@gmail.com>
2024-07-24 01:03:11 +0700

    hoàn thiện hiển thị sản phẩm trang Home

--- src/main/resources/static/js/Home.js
+++ src/main/resources/static/js/Home.js
@@ -24,48 +24,146 @@ window.addEventListener('resize', () => {
 
 
 // #################################################################
-// main -> section -> nhom 1
+// main -> section -> nhom 1, 2, 3
 async function fetchProducts() {
-    const boxElement = document.querySelector('.box-product');
+    const shoesElement = document.querySelector('.box-product');
+    const bagElement = document.querySelector('.bag');
+    const clothesElement = document.querySelector('.clothes');
 
     var count_shoes = 10;
+    var count_bags = 10;
+    var count_clothes = 10;
     var product_name = '';
 
     const response = await fetch('/api/products');
     const products = await response.json();
 
     for(let i=0; i<products.length; i++) {
-        if(products[i].ten !== product_name) {
-            const product_item = `
+        if(products[i].ten !== product_name && products[i].loai === "giày" && count_shoes > 0) {
+            // Sắp xếp mảng photoNames
+            products[i].photoNames.sort((a, b) => a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }));
+
+            var product_item = `
             <div class="col l-1-2 m-1-2 c-1-2 product">
                 <div class="product-item">
-                    <a href="/neekine">
-                        <div class="product-img">
-                            <span class="percent">${products[i].phan_tram}%</span>
-                            <img src="/images/${products[i].photoNames[0]}" alt="" class="first-img">
+                    <a th:href="@{/neekine}">
+                        <div class="product-img">`
+            
+            if(products[i].phan_tram !== 0){
+                product_item += `<span class="percent">${products[i].phan_tram}%</span>`;
+            }
+
+            product_item += `<img src="/images/${products[i].photoNames[0]}" alt="" class="first-img">
                             <img src="/images/${products[i].photoNames[1]}" alt="" class="second-img">
                         </div>
                         <p>${products[i].ten}</p>
-                    </a>
+                    </a>`
 
-                    <div class="cost">
-                        <span class="price">${products[i].gia_ban * (products[i].phan_tram/100)} đ</span>
-                        <span class="old-price">${products[i].gia_ban} đ</span>
-                    </div>
-                </div>
+            if(products[i].phan_tram !== 0){
+                product_item += ` <div class="cost">
+                            <span class="new-price">${formatCurrency(products[i].gia_ban * (products[i].phan_tram/100))} đ</span>
+                            <span class="old-price">${formatCurrency(products[i].gia_ban)} đ</span>
+                        </div>`
+            }
+            else{
+                product_item += ` <div class="cost">
+                            <span class="price">${products[i].gia_ban.toLocaleString('vi-VN')} đ</span>
+                        </div>`
+            }
+                `</div>
             </div>
             `;
-            boxElement.innerHTML += product_item;
+            shoesElement.innerHTML += product_item;
 
             product_name = products[i].ten;
             count_shoes--;
+        }
+        else if(products[i].ten !== product_name && products[i].loai === "bag" && count_bags > 0) {
+            // Sắp xếp mảng photoNames
+            products[i].photoNames.sort((a, b) => a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }));
 
-            if(count_shoes == 0) {
-                break;
+            var product_item = `
+            <div class="col l-2-4 m-6 c-6 product">
+                <div class="product-item">
+                    <a th:href="@{/neekine}">
+                        <div class="product-img">`
+            
+            if(products[i].phan_tram !== 0){
+                product_item += `<span class="percent">${products[i].phan_tram}%</span>`;
             }
+
+            product_item += `<img src="/images/${products[i].photoNames[0]}" alt="" class="first-img">
+                            <img src="/images/${products[i].photoNames[1]}" alt="" class="second-img">
+                        </div>
+                        <p>${products[i].ten}</p>
+                    </a>`
+
+            if(products[i].phan_tram !== 0){
+                product_item += ` <div class="cost">
+                            <span class="new-price">${formatCurrency(products[i].gia_ban * (products[i].phan_tram/100))} đ</span>
+                            <span class="old-price">${formatCurrency(products[i].gia_ban)} đ</span>
+                        </div>`
+            }
+            else{
+                product_item += ` <div class="cost">
+                            <span class="price">${formatCurrency(products[i].gia_ban)} đ</span>
+                        </div>`
+            }
+                `</div>
+            </div>
+            `;
+            bagElement.innerHTML += product_item;
+
+            product_name = products[i].ten;
+            count_bags--;
+        }
+        else if(products[i].ten !== product_name && products[i].loai === "clothes" && count_clothes > 0) {
+            // Sắp xếp mảng photoNames
+            products[i].photoNames.sort((a, b) => a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }));
+
+            var product_item = `
+            <div class="col l-2-4 m-6 c-6 product">
+                <div class="product-item">
+                    <a th:href="@{/neekine}">
+                        <div class="product-img">`
+            
+            if(products[i].phan_tram !== 0){
+                product_item += `<span class="percent">${products[i].phan_tram}%</span>`;
+            }
+
+            product_item += `<img src="/images/${products[i].photoNames[0]}" alt="" class="first-img">
+                            <img src="/images/${products[i].photoNames[1]}" alt="" class="second-img">
+                        </div>
+                        <p>${products[i].ten}</p>
+                    </a>`
+            
+            if(products[i].phan_tram !== 0){
+                product_item += ` <div class="cost">
+                            <span class="new-price">${formatCurrency(products[i].gia_ban * (products[i].phan_tram/100))} đ</span>
+                            <span class="old-price">${formatCurrency(products[i].gia_ban)} đ</span>
+                        </div>`
+            }
+            else{
+                product_item += ` <div class="cost">
+                            <span class="price">${formatCurrency(products[i].gia_ban)} đ</span>
+                        </div>`
+            }
+                    
+                `</div>
+            </div>
+            `;
+            clothesElement.innerHTML += product_item;
+
+            product_name = products[i].ten;
+            count_clothes--;
+        }
+        
+        if(count_shoes === 0 && count_bags === 0 && count_clothes === 0){
+            break;
         }
     }
 
+    // xử lý sự kiện chuyển shoes products
     const slider = document.querySelector('.box-product');
     const nextButton = document.querySelector('.next');
     const prevButton = document.querySelector('.prev');
@@ -89,74 +187,12 @@ async function fetchProducts() {
         slider.style.transform = `translateX(-${index * 10}%)`;
     }
 }
+
+function formatCurrency(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 document.addEventListener('DOMContentLoaded', fetchProducts);
-
-
-// main -> section -> nhom 2,3
-document.addEventListener('DOMContentLoaded', function() {
-    const bagElement = document.querySelector('.bag');
-    const clothesElement = document.querySelector('.clothes');
-
-    // Lấy chiều rộng của toàn bộ trang (viewport)
-    const viewportWidth = window.innerWidth;
-    var count_bag = 8;
-    var count_clothes = 7;
-
-    if(viewportWidth <= 1112){
-        count_clothes = 6;
-    }
-    else {
-        count_clothes = 7;
-    }
-
-    for(let i=2; i<count_bag; i++){
-        const product_item = `
-            <div class="col l-2-4 m-6 c-6 product">
-                <div class="product-item">
-                    <a href="/neekine">
-                        <div class="product-img">
-                            <span class="percent">20%</span>
-                            <img src="/images/balo-${i+1}.1.jpg" alt="" class="first-img">
-                            <img src="/images/balo-${i+1}.2.jpg" alt="" class="second-img">
-                        </div>
-                        <p>New Balance Fresh Foam Arishi v3 Slip Resistant - Grey</p>
-                    </a>
-
-                    <div class="cost">
-                        <span class="price">1,000,000 đ</span>
-                        <span class="old-price">1,250,000 đ</span>
-                    </div>
-                </div>
-            </div>
-        `;
-
-        bagElement.innerHTML += product_item;
-    }
-
-    for(let i=0; i<count_clothes; i++){
-        const product_item = `
-        <div class="col l-2-4 m-6 c-6 product">
-            <div class="product-item">
-                <a href="/neekine">
-                    <div class="product-img">
-                        <span class="percent">20%</span>
-                        <img src="/images/coat-${i+1}.1.jpg" alt="" class="first-img">
-                        <img src="/images/coat-${i+1}.2.jpg" alt="" class="second-img">
-                    </div>
-                    <p>New Balance Fresh Foam Arishi v3 Slip Resistant - Grey</p>
-                </a>
-
-                <div class="cost">
-                    <span class="price">1,000,000 đ</span>
-                    <span class="old-price">1,250,000 đ</span>
-                </div>
-            </div>
-        </div>
-        `;
-
-        clothesElement.innerHTML += product_item;
-    }
-});
 
 // body -> body-search-pc
 // thao tác ẩn hiện search-pc
