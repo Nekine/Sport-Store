5b0d8500abf972fa158eddd7a23e7fa11f5e4b1f
Nekine <kien12045@gmail.com>
2024-07-28 17:26:27 +0700

    lỗi hiển thị sản phẩm

--- src/main/java/vn/neekine/shoes_store_website/service/ProductService.java
+++ src/main/java/vn/neekine/shoes_store_website/service/ProductService.java
@@ -2,9 +2,13 @@ package vn.neekine.shoes_store_website.service;
 
 import java.util.List;
 
-import vn.neekine.shoes_store_website.DTO.ProductDTO;
+import org.springframework.data.domain.Page;
+
+import vn.neekine.shoes_store_website.DTO.ProductDetailsDTO;
+import vn.neekine.shoes_store_website.model.SanPham;
 
 public interface ProductService {
-    public List<ProductDTO> getAllProducts();
-    public List<ProductDTO> searchProduct(String name);
+    public List<ProductDetailsDTO> getAllProducts();
+    public List<ProductDetailsDTO> searchProduct(String name);
+    public Page<ProductDetailsDTO> getAllPages(int integer, int size);
 }
